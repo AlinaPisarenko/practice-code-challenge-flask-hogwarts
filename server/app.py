952347ff2fc088bd5c522bada2836fd1250beeaa
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-06-29 09:02:01 -0400

    solution

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Student, Subject, SubjectEnrollment
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -17,6 +17,96 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+api=Api(app)
+
+class Students(Resource):
+    def get(self):
+        students = Student.query.all()
+        response_body = [student.to_dict(rules=('-subject_enrollments',)) for student in students]
+
+        return make_response(jsonify(response_body), 200)
+
+api.add_resource(Students, '/students')
+
+
+class StudentsById(Resource):
+    def get(self, id):
+        student = Student.query.filter(Student.id == id).first()
+
+        if not student:
+            response_body = {'error': 'Student not found'}
+            return make_response(jsonify(response_body), 404)
+
+        return make_response(jsonify(student.to_dict()), 200)
+
+api.add_resource(StudentsById, '/students/<int:id>')
+
+
+class Subjects(Resource):
+    def get(self):
+        subjects = Subject.query.all()
+        response_body = [subject.to_dict(rules=('-subject_enrollments',)) for subject in subjects]
+
+        return make_response(jsonify(response_body), 200)
+
+api.add_resource(Subjects, '/subjects')
+
+class SubjectsById(Resource):
+    def get(self, id):
+        subject = Subject.query.get(id)
+
+        if not subject:
+            response_body = {'error': 'Subject not found'}
+            return make_response(jsonify(response_body), 404)
+
+        return make_response(jsonify(subject.to_dict()), 200)
+
+api.add_resource(SubjectsById, '/subjects/<int:id>')
+
+class SubjectEnrollments(Resource):
+    def post(self):
+        try:
+            json_data=request.get_json()
+
+            new_enrollment = SubjectEnrollment(
+                enrollment_year=json_data.get('enrollment_year'),
+                student_id=json_data.get('student_id'),
+                subject_id=json_data.get('subject_id')
+                )
+            
+            db.session.add(new_enrollment)
+            db.session.commit()
+
+            response_body = new_enrollment.to_dict()
+
+            return make_response(jsonify(response_body), 200)
+
+        except ValueError:
+            response_body = { "errors": ["validation errors"] }
+
+            return make_response(jsonify(response_body), 400)
+
+
+api.add_resource(SubjectEnrollments, '/subject_enrollments')
+
+class SubjectEnrollmentsById(Resource):
+    def delete(self, id):
+
+        subject_enrollment  = SubjectEnrollment.query.get(id)
+
+        if not subject_enrollment:
+            response_body = {'error': 'Subject Enrollment not found'}
+            return make_response(jsonify(response_body), 404)
+
+        db.session.delete(subject_enrollment)
+        db.session.commit()
+
+        return make_response({}, 204)
+    
+            
+
+api.add_resource(SubjectEnrollmentsById, '/subject_enrollments/<int:id>')
 
 @app.route('/')
 def home():
--- server/models.py
+++ server/models.py
@@ -18,11 +18,27 @@ class Student(db.Model, SerializerMixin):
     age = db.Column(db.String)
 
     # Add relationship
+    subject_enrollments = db.relationship('SubjectEnrollment', back_populates='student', cascade='all, delete-orphan')
+    subjects = association_proxy('subject_enrollments', 'subject', creator = lambda s: SubjectEnrollment(subject=s))
     
     # Add serialization
+    serialize_rules = ('-subject_enrollments.student',)
 
     # Add validation
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError(f'Must have a {key}')
 
+        return value
+
+
+    @validates('age')
+    def validates_age(self,key,value):
+        if not (11 <= value <= 18):    
+            raise ValueError(f'Must have {key} between 11 and 18 years old')
+
+        return value
     
     
     def __repr__(self):
@@ -35,8 +51,11 @@ class Subject(db.Model, SerializerMixin):
     title = db.Column(db.String)
 
     # Add relationship
-    
+    subject_enrollments = db.relationship('SubjectEnrollment', back_populates='subject', cascade='all,delete-orphan')
+    students = association_proxy('subject_enrollments', 'student', creator = lambda s: SubjectEnrollment(student=s))
+
     # Add serialization
+    serialize_rules = ('-subject_enrollments.subject',)
     
     def __repr__(self):
         return f'<Subject {self.id}>'
@@ -47,11 +66,26 @@ class SubjectEnrollment(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     enrollment_year = db.Column(db.Integer, nullable=False)
 
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+
     # Add relationships
+    student = db.relationship('Student', back_populates='subject_enrollments')
+    subject = db.relationship('Subject', back_populates='subject_enrollments')
     
     # Add serialization
+    serialize_rules = ('-student','-subject')
     
     # Add validation
+    @validates('enrollment_year')
+    def validates_enrollment_year(self, key, value):
+        if value > 2023:
+            raise ValueError(f'Must have {key} before 2023')
+
+        return value
+
+
+    
     
     def __repr__(self):
         return f'<SubjectEnrollment {self.id}>'
